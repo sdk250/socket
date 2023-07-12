2d901ef15eb5920b6b5ad23ea5215c223f3918bd
sdk250 <520sdk250@gmail.com>
2023-07-12 15:01:11 +0800

    None

--- arg.c
+++
@@ -1,4 +0,0 @@
-#include <stdio.h>
-int main(int argc, char** argv){puts(argv[1]);
-return 0;
-}
--- server.c
+++ server.c
@@ -4,7 +4,7 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-#define SIZE 0xFF
+#define SIZE 0x2FF
 
 int main(void) {
 	int fd = 0;
@@ -25,9 +25,10 @@ int main(void) {
 	int i = 0;
 	i = accept(fd, (struct sockaddr*)&client, &len);
 	printf("Accept function return: %d\nAddress: %s:%u\n", i, inet_ntoa(client.sin_addr), ntohs(client.sin_port));
-	for (int i = 0, total = 0; (i = read(fd, buffer + total, SIZE - total)); total += i > 0 ? i : 0) {
-		printf("Read: %dbytes.\n", i);
-	}
+	char* response = "HTTP/1.1 200 OK\r\nServer: C/Socket\r\n\r\n<h1><b><i>Hello</i></b></h1>";
+	printf("Read: %zd bytes.\n", read(i, buffer, SIZE));
+	printf("Sended: %zd bytes\n", write(i, response, strlen(response)));
+
 	puts(buffer);
 
 	close(fd);
