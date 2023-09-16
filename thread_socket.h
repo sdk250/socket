82192210ba3878e2bffca5908ee6c67dc9d97a0d
sdk250 <520sdk250@gmail.com>
2023-09-16 16:45:44 +0800

    尝试使用标准http请求写法来处理http请求

--- thread_socket.h
+++ thread_socket.h
@@ -15,16 +15,6 @@
 #define TIMEOUT 3
 #define SERVER_ADDR "153.3.236.22"
 
-long int recv_headers(int, char *, size_t);
-int setNonBlocking(int);
-void *handle_connection(void *);
-void set_socket_timeout(int, unsigned long int, unsigned int);
-void *client_to_server(void *);
-void *server_to_client(void *);
-void main_loop(int);
-void usage(const char *, int);
-void signal_terminate(int);
-
 extern pthread_attr_t attr;
 extern int LOG;
 extern int local_fd;
@@ -35,4 +25,20 @@ struct arg {
     char *buf;
     int http;
 };
+
+struct http_header {
+	char *data;
+	struct http_header *prev;
+	struct http_header *next;
+};
+
+long int recv_headers(int, struct http_header *, size_t);
+int setNonBlocking(int);
+void *handle_connection(void *);
+void set_socket_timeout(int, unsigned long int, unsigned int);
+void *client_to_server(void *);
+void *server_to_client(void *);
+void main_loop(int);
+void usage(const char *, int);
+void signal_terminate(int);
 
